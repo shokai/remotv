68f7fc29d5d5bf3cb5197ef1487d8b97be646676
Sho Hashimoto <hashimoto@shokai.org>
2013-02-02 14:36:54 +0900

    use xframe.js  #1

--- public/js/tv.js
+++ public/js/tv.js
@@ -1,4 +1,9 @@
 var io = new CometIO().connect();
+var tv = null;
+
+$(function(){
+  tv = new XFrame("div#tv");
+});
 
 io.on("connect", function(session){
   console.log("connect!! "+session);
@@ -9,5 +14,10 @@ io.on("error", function(err){
 });
 
 io.on("go", function(data){
-  $("iframe#tv").attr("src", data.url);
+  console.log("load "+data.url);
+  tv.load(data.url)
+});
+
+io.on("scroll", function(data){
+  console.log(data);
 });
