59329e52657cfe352706bd998348f3d3275f9911
Sho Hashimoto <hashimoto@shokai.org>
2013-04-05 12:08:52 +0900

    show channels on index page #2

--- helpers/helper.rb
+++ helpers/helper.rb
@@ -6,4 +6,10 @@ helpers do
   def app_root
     "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}#{env['SCRIPT_NAME']}"
   end
+
+  def channels
+    Sinatra::RocketIO.channels.values.uniq.map{|i|
+      Channel.parse(i).to_s
+    }.uniq.sort
+  end
 end
